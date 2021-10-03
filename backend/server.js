1c01759b5b2151667182e21918e967f702ac79aa
anishpunaroor <anish.punaroor@gmail.com>
2021-10-03 15:40:03 -0700

    Init server and db

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,11 @@
-import path from "path";
-import express from "express"; 
-import dotenv from "dotenv"; 
-import colors from "colors";
-import morgan from "morgan";
+import express from "express";
+//mport dotenv from "dotenv";
+//import morgan from "morgan";
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
 
-app.use(express.json());
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-
-
-
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow
-      .bold
-  )
-);
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`)
+})
