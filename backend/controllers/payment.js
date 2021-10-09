196718378e1959c4de2b1ca14573216a9980b810
anishpunaroor <anish.punaroor@gmail.com>
2021-10-08 17:10:01 -0700

    Braintree gateway

---
+++ backend/controllers/payment.js
@@ -0,0 +1,51 @@
+import braintree from "braintree";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Setup payment gateway with braintree
+const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    merchantId: process.env.MERCHANT_ID,
+    publicKey: process.env.PUBLIC_KEY,
+    privateKey: process.env.PRIVATE_KEY,
+});
+
+// Create payment token 
+export const getToken = (req, res) => {
+    gateway.clientToken.generate({}, (err, response) => {
+        if (err) {
+            res.status(500);
+            throw new Error(err.message);
+        } else {
+            res.send(response);
+        }
+    });
+};
+
+export const processPayment = (req, res) => {
+    let clientNonce = req.body.paymentMethodNonce;
+    let clientAmount = req.body.amount;
+    let orderId = req.body.orderId;
+    gateway.merchantAccount.createForCurrency({
+        currency: "USD",
+    });
+    gateway.transaction.sale(
+        {
+            orderId: orderId,
+            amount: clientAmount,
+            paymentMethodNonce: clientNonce,
+            options: {
+                submitForSettlement: true,
+            },
+        },
+        (err, result) => {
+            if (err) {
+                res.status(500);
+                throw new Error(err.message);
+            } else {
+                res.json(result);
+            }
+        }
+    );
+};
