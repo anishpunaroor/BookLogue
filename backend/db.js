f6c46366e554025a04a724f0a669407abdaebece
anishpunaroor <anish.punaroor@gmail.com>
2021-10-10 23:37:54 -0700

    Fix MongoDB connection

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {});
 
-    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
-  } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline);
-    process.exit(1);
-  }
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
