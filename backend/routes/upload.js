6bd37e553c495f1ca14af3f9efe5fba5cd9464ff
anishpunaroor <anish.punaroor@gmail.com>
2021-10-10 22:42:14 -0700

    Image upload route

---
+++ backend/routes/upload.js
@@ -0,0 +1,46 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+
+const router = express.Router();
+
+// Store images in disk storage
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, "uploads/");
+    },
+    filename(req, file, cb) {
+        cb(
+            null,
+            `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+        );
+    },
+});
+
+// Check the type of file extension
+function checkFileType(file, cb) {
+    const filetypes = /jpg|jpeg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        return cb(null, true);
+    } else {
+        return cb("Images only");
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    },
+});
+
+// Upload image to express app
+router.post("/", upload.single("image"), (req, res) => {
+    res.send(`/${req.file.path}`);
+});
+
+export default router;
